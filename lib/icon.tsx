c247cc45281cd2e1bb6654ff21253103914b0e27
pzjyyzs <463669395@qq.com>
2020-09-02 23:22:46 +0800

    sass-load

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -1,16 +1,15 @@
 import * as React from 'react';
-import './importIcons.js'
+import './importIcons.js';
+import './icon.scss';
 export interface IconProps {
     name: string;
 }
  
 const Icon: React.SFC<IconProps> = (props) => {
     return (
-   <span>
-       <svg>
+       <svg className="pzui-icon">
            <use xlinkHref={`#${props.name}`}></use>
        </svg>
-   </span>
       );
 }
  
