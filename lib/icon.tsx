b3a05d0ba12f2252cfe421c7c88b6ed85f2a4079
pzjyyzs <463669395@qq.com>
2020-09-03 23:15:52 +0800

    点击事件类型

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -3,11 +3,12 @@ import './importIcons.js';
 import './icon.scss';
 export interface IconProps {
     name: string;
+    onClick: React.MouseEventHandler<SVGElement>
 }
  
 const Icon: React.SFC<IconProps> = (props) => {
     return (
-       <svg className="pzui-icon">
+       <svg className="pzui-icon" onClick={props.onClick}>
            <use xlinkHref={`#${props.name}`}></use>
        </svg>
       );
--- lib/index.tsx
+++ lib/index.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Icon from './icon';
 
-
+const fn: React.MouseEventHandler = (e) => {
+    console.log(e)
+}
 ReactDOM.render(
-    <Icon name='add-outline'></Icon>,
+    <Icon name='add-outline' onClick={fn}></Icon>,
     document.querySelector('#root')
 );
