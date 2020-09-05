97be84fc14c7de54fc583e9624afe358d36b33fb
pzjyyzs <463669395@qq.com>
2020-09-05 15:48:54 +0800

    icon 测试

--- lib/helpers/__tests__/classes.unit.jsx
+++
@@ -1,7 +0,0 @@
-import classes from '../classes'
-describe('classes', () => {
-    it('接受一个className', () => {
-        const result  = classes('a')
-        expect(result).toEqual('a')
-    })
-});
