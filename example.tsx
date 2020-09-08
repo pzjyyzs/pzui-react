740cafbcc9cae2e0ff0216e46492d4738fad0ffb
pzjyyzs <463669395@qq.com>
2020-09-08 23:04:07 +0800

    创立一个简单官网

--- example.tsx
+++ example.tsx
@@ -1,23 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {HashRouter as Router, Route,Link} from 'react-router-dom';
 import Icon from './lib/icon/icon'
+import IconExample from 'lib/icon/icon.example';
 
 ReactDOM.render((
-    <div>
-        <header>
-            <div className="logo">
-                PZUI
-            </div>
-        </header>
+    <Router>
         <div>
-            <aside>
-                <h2>组件</h2>
-                <ul>
-                    <li>Icon</li>
-                </ul>
-            </aside>
-            <main></main>
+            <header>
+                <div className="logo">
+                    PZUI
+                </div>
+            </header>
+            <div>
+                <aside>
+                    <h2>组件</h2>
+                    <ul>
+                        <li>
+                            <Link to="/icon">Icon</Link>
+                        </li>
+                    </ul>
+                </aside>
+                <main>
+                    <Route path="/icon" component={IconExample}></Route>
+                </main>
+            </div>
+            <Icon name="add-outline"></Icon>
         </div>
-        <Icon name="add-outline"></Icon>
-    </div>
+    </Router>
 ), document.querySelector('#root'))
---
+++ lib/icon/icon.example.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const IconExample:React.FunctionComponent = () => {
+    return (
+    <div>h1</div>
+    )
+}
+
+export default IconExample
