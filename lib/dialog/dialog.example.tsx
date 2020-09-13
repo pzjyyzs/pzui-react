83959ae66e8b87dd4b7293b61a13759e7581f7d1
pzjyyzs <463669395@qq.com>
2020-09-13 22:15:10 +0800

    dialog

--- lib/dialog/dialog.example.tsx
+++ lib/dialog/dialog.example.tsx
@@ -1,19 +1,28 @@
 import React, { useState } from 'react';
-import Dialog from './dialog';
+import Dialog, {alert } from './dialog';
 
 export default function() {
     const [x, setX] = useState(false);
     return (
         <div>
-            <button onClick={()=>setX(!x)}>click</button>
-            <Dialog visible={x} buttons={
-                [
-                    <button onClick={() => setX(false)}>1</button>,
-                    <button onClick={() => setX(false)} >2</button>
-                ]
-            }>
-                <div>childrens</div>
-            </Dialog>
+            <div>
+                <button onClick={()=>setX(!x)}>click</button>
+                <Dialog visible={x} buttons={
+                    [
+                        <button onClick={() => setX(false)}>1</button>,
+                        <button onClick={() => setX(false)} >2</button>
+                    ]
+                } onClose={() => {
+                    setX(false)
+                }}>
+                    <div>childrens</div>
+                </Dialog>
+            </div>
+
+            <div>
+                <button onClick={() => alert('1')}>alert</button>
+                <button onClick={() => alert('1')}>alert</button>
+            </div>
         </div>
     )
 }
--- lib/dialog/dialog.tsx
+++ lib/dialog/dialog.tsx
@@ -1,38 +1,102 @@
-import React, { Fragment, ReactElement } from 'react';
+import React, { Fragment, ReactElement, ReactFragment, ReactNode } from 'react';
 import './dialog.scss';
 import Icon from '../icon/icon';
 import { scopeClassMaker } from '../classes';
+import ReactDOM from 'react-dom';
 
 interface Props {
     visible: boolean;
-    buttons: Array<ReactElement>
+    buttons?: Array<ReactElement>;
+    onClose: React.MouseEventHandler;
+    closeOnClickMask?: boolean;
 }
 
 const scopedClass = scopeClassMaker('fui-dialog');
 
 const Dialog: React.FunctionComponent<Props> = (props) => {
-    
-    return (
-        props.visible ?
-        <Fragment>
-            <div className={scopedClass('mask')}></div>
-            <div className={scopedClass()}>
-                <div className={scopedClass('close')}>
-                    <Icon name='close-outline'></Icon>
-                </div>
-                <header className={scopedClass('header')}>
-                    提示
-                </header>
-                <main className={scopedClass('main')}>
-                    {props.children}
-                </main>
-                <footer className={scopedClass('footer')}>
-                   {props.buttons}
-                </footer>
+    const onClickClose: React.MouseEventHandler = (e) => {
+        props.onClose(e)
+    }
+    const onClickMask: React.MouseEventHandler = (e) => {
+        if (props.closeOnClickMask) {
+            props.onClose(e);
+        }
+    }
+
+    const result = props.visible ?
+    <Fragment>
+        <div className={scopedClass('mask')} onClick={onClickMask}></div>
+        <div className={scopedClass()}>
+            <div className={scopedClass('close')} onClick={onClickClose}>
+                <Icon name='close-outline'></Icon>
             </div>
-        </Fragment> :
-        null
+            <header className={scopedClass('header')}>
+                提示
+            </header>
+            <main className={scopedClass('main')}>
+                {props.children}
+            </main>
+            <footer className={scopedClass('footer')}>
+               {props.buttons && props.buttons.map((button, index) => 
+                React.cloneElement(button, { key: index})
+                )}
+            </footer>
+        </div>
+    </Fragment> :
+    null
+    return (
+        ReactDOM.createPortal(result, document.body)
     )
 }
 
+Dialog.defaultProps = {
+    closeOnClickMask: false
+}
+
+const modal = (content: ReactNode, buttons?: Array<ReactElement>, afterClose?:() => void) => {
+    const close = () => {
+        ReactDOM.render(React.cloneElement(component, { visible: false}), div);
+        ReactDOM.unmountComponentAtNode(div);
+        div.remove();
+        
+    }
+    const component = <Dialog 
+                        onClose={() => {
+                            close();
+                            afterClose && afterClose();
+                        }} 
+                        visible={true} 
+                        buttons = {buttons}>
+        {content}
+    </Dialog>
+    const div = document.createElement('div');
+    document.body.append(div);
+    ReactDOM.render(component, div);
+    return close;
+}
+
+const alert = (content: string) => {
+    const button = [<button onClick={() => {close}}>ok</button>];
+    const close = modal(content, button)
+}
+
+const confirm = (content: string, yes?: ()=>void, no?: () => void) => {
+    const onYes = () => {
+        close()
+        yes && yes();
+    }
+    const onNo = () => {
+        close()
+        no && no()
+    }
+    const buttons = [
+        <button onClick={onYes}>yes</button>,
+        <button onClick={onNo}>no</button>
+    ]
+    const close = modal(content, buttons,no)
+};
+
+
+
+export { alert, confirm, modal } ;
 export default Dialog;
