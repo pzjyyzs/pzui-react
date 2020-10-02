a6d10002e39dce5514b61188c89ecb9cace6cb2e
pzjyyzs <463669395@qq.com>
2020-10-03 03:06:20 +0800

    form 异步问题修改

--- lib/form/validator.tsx
+++ lib/form/validator.tsx
@@ -5,10 +5,7 @@ interface FormRule {
     minLength?: number;
     maxLength?: number;
     pattern?: RegExp;
-    validate?: {
-        name: string;
-        validator:(value: string) => Promise<void>
-    }
+    validator?: (value: string) => Promise<string>;
 }
 
 
@@ -22,10 +19,8 @@ export function noError(errors: any) {
     return Object.keys(errors).length === 0;
 }
 
-interface OneError {
-    message: string;
-    promise?: Promise<any>;
-}
+type OneError =  string | Promise<string>;
+
 const Validator = (formValue: FormValue, rules: FormRules, callback: (errors: any) =>void, ): void => {
     let errors: any = { }
     const addErrors = (key: string, error: OneError) => {
@@ -36,45 +31,37 @@ const Validator = (formValue: FormValue, rules: FormRules, callback: (errors: an
     }
     rules.map(rule => {
         const value = formValue[rule.key];
-        if (rule.validate) {
-            const promise = rule.validate.validator(value);
-            addErrors(rule.key, { message: '', promise})
+        if (rule.validator) {
+            const promise = rule.validator(value);
+            addErrors(rule.key, promise)
         }
         if (rule.required) {
             if (value === undefined && value === null || value === '') {
-                addErrors(rule.key, { message: '必填'})
+                addErrors(rule.key, '必填')
             }
         }
         if (rule.minLength) {
             if (!isEmpty(value) && value.length < rule.minLength) {
-                addErrors(rule.key, { message: '短'})
+                addErrors(rule.key, '短')
             }
         }
         if (rule.pattern) {
             if (!(rule.pattern.test(value))) {
-                addErrors(rule.key, { message: '格式不正确'});
+                addErrors(rule.key, '格式不正确');
             }
         }
     })
-    const promiseList = flat(Object.values(errors))
-    .filter(item => item.promise)
-    .map(item => item.promise)
-    Promise.all(promiseList)
-    .then(() => {
-        const newErrors =fromEntries(
-            Object.keys(errors)
-            .map<[string, string[]]>(key => {
-               return [key, errors[key].map((item: OneError) =>  item.message)]
-            }));
-        callback(newErrors)
-    }, () => {
-        const newErrors =fromEntries(
-            Object.keys(errors)
-            .map<[string, string[]]>(key => {
-               return [key, errors[key].map((item: OneError) =>  item.message)]
-            }));
-        callback(newErrors)
-    });
+
+    const keyAndPromise = Object.keys(errors).map(key => 
+        errors[key].map((promise: any) => [key, promise])
+    )
+    const flattenErrors = flat(keyAndPromise);
+    const promiseArray = flattenErrors.map(([key, promiseOrString]) => 
+    (promiseOrString instanceof Promise ? promiseOrString: Promise.reject(promiseOrString))
+    .then(() =>  [key, undefined], (reason) => [key, reason]))
+    Promise.all(promiseArray).then((results: Array<[string, string]>) => {
+        callback(zip(results.filter(item => item[1])))
+    })
 }
 
 export default Validator;
@@ -96,5 +83,14 @@ function fromEntries(array: Array<[string, string[]]>) {
     for(let i = 0;i<array.length;i++) {
         result[array[i][0]] = array[i][1]
     }
+    return result;
+}
+
+function zip(kvList: Array<[string, string]>) {
+    const result:{[K: string]: Array<string>} = {};
+    kvList.map(([key, value]) => {
+        result[key] =  result[key] || [];
+        result[key].push(value);
+    });
     return result;
 }
