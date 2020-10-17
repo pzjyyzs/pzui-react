15e03247677fd44f56d041a7d0ddcc42705a3103
pzjyyzs <463669395@qq.com>
2020-10-17 14:05:19 +0800

    代码高亮

--- lib/icon/icon.example.tsx
+++ lib/icon/icon.example.tsx
@@ -1,12 +1,11 @@
+import Icon  from './icon';
 import React from 'react';
 
-const x = require('!!raw-loader!./icon.example.tsx');
+
 const IconExample:React.FunctionComponent = () => {
     return (
     <div>
-        <pre>
-            {x.default}
-        </pre>
+        <Icon name='add-outline'></Icon>
     </div>
     )
 }
