96c2236c09d0c26860faac09a2330c1cab8b168f
pzjyyzs <463669395@qq.com>
2020-10-21 22:45:12 +0800

    button 样式

--- lib/button/button.example.tsx
+++ lib/button/button.example.tsx
@@ -5,17 +5,21 @@ import './button.example.scss';
 const ButtonExample:React.FunctionComponent = () => {
     return (
         <div>
-
             <div className='row'>
                 <Button>默认图标</Button>
-                <Button type='primary'>主要按钮</Button>
-                <Button type='success'>成功按钮</Button>
-                <Button type='warning'>警告按钮</Button>
-                <Button type='danger'>危险按钮</Button>
-                <Button type='info'>信息按钮</Button>
+                <Button color='primary'>主要按钮</Button>
+                <Button color='success'>成功按钮</Button>
+                <Button color='warning'>警告按钮</Button>
+                <Button color='danger'>危险按钮</Button>
+                <Button color='info'>信息按钮</Button>
             </div>
             <div className='row'>
                 <Button disabled>默认图标</Button>
+                <Button color='primary' disabled>主要按钮</Button>
+                <Button color='success' disabled>成功按钮</Button>
+                <Button color='warning' disabled>警告按钮</Button>
+                <Button color='danger' disabled>危险按钮</Button>
+                <Button color='info' disabled>信息按钮</Button>
             </div>
         </div>
     )
--- lib/button/button.tsx
+++ lib/button/button.tsx
@@ -1,26 +1,27 @@
-import React, { HTMLAttributes } from 'react';
+import React, { ButtonHTMLAttributes } from 'react';
 import { scopeClassMaker } from '../helpers/classes';
 import './button.scss';
 
 const sc = scopeClassMaker('pzui-button');
 
-interface ButtonProps extends HTMLAttributes<HTMLElement> {
-    type?: string;
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+    color?: string;
     disabled?: boolean;
 }
 
 const Button:React.FunctionComponent<ButtonProps> = (props) => {
-    const { className, children,  type, disabled, ...rest} = props;
+    const { className, children,  color, disabled, ...rest} = props;
     const cssDisabled = disabled ? 'pzui-button-disabled' : '';
+    const classAll =className ? className : '';
     return (
-        <div className={sc('',{extra: `${cssDisabled} pzui-button-${type}`})} {...rest}>
+        <button className={sc('',{extra: `${cssDisabled} pzui-button-${color} ${classAll}`})} {...rest}>
             {children}
-        </div>
+        </button>
     )
 }
 
 Button.defaultProps = {
-    type: 'initial',
+    color: 'initial',
     disabled: false
 }
 export default Button;
